68bbf37542d3347d26b0b768e1ba6a7bdc0caf4b
Gunnar Omander <gunnar.omander@gmail.com>
2023-01-25 00:06:13 +0100

    Initial commit

---
+++ client/src/components/models.ts
@@ -0,0 +1,13 @@
+import {
+  DestinyInventoryComponent,
+  DestinyInventoryItemDefinition
+} from 'bungie-api-ts/destiny2'
+
+export interface Todo {
+  id: number
+  content: string
+}
+
+export interface Meta {
+  totalCount: number
+}
