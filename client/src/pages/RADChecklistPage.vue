15ecb6dd0a5a4d4b6ddcad0a692bb03dbb071b3d
Gunnar Omander <gunnar.omander@gmail.com>
2023-06-21 11:29:42 -0700

    Add buttons to other raid pages

--- client/src/pages/RADChecklistPage.vue
+++ client/src/pages/RADChecklistPage.vue
@@ -1,20 +1,28 @@
 <template>
-  <q-page class="q-pa-md">
+  <q-page class="q-pa-md q-gutter-y-md">
     <h1>Raid and Dungeon Checklist</h1>
 
-    <div v-if="$route.params.id">
-      {{ $route.params.id }}
+    <div v-if="route.params.id">
+      {{ route.params.id }}
     </div>
 
     <div v-else>
       <q-btn
         label="Create group"
         no-caps
+        color="primary"
       />
     </div>
 
-    <div>
-      Links to other raids here
+    <div class="row q-gutter-sm">
+      <q-btn
+        v-for="raid of raids"
+        :label="raid.title"
+        no-caps
+        color="primary"
+        :to="`${path}/${raid.id}`"
+        :disable="route.path.includes(raid.id)"
+      />
     </div>
 
     <router-view/>
@@ -22,5 +30,41 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+
+const path = computed(() => `/rad-checklist${route.params.id ? `/${route.params.id}` : ''}`)
+
+const raids = [
+  {
+    title: 'Last Wish',
+    id: 'last-wish'
+  },
+  {
+    title: 'Garden of Salvation',
+    id: 'garden-of-salvation'
+  },
+  {
+    title: 'Deep Stone Crypt',
+    id: 'deep-stone-crypt'
+  },
+  {
+    title: 'Vault of Glass',
+    id: 'vault-of-glass'
+  },
+  {
+    title: 'Vow of the Disciple',
+    id: 'vow-of-the-disciple'
+  },
+  {
+    title: 'King\'s Fall',
+    id: 'kings-fall'
+  },
+  {
+    title: 'Root of Nightmares',
+    id: 'root-of-nightmares'
+  }
+]
 </script>
