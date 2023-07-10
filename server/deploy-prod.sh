5eb4b365a6fb7c7ec8a3f51d4e8dcb682032a284
Gunnar Omander <gunnar.omander@gmail.com>
2023-07-11 01:04:31 +0200

    Set v11

---
+++ server/deploy-prod.sh
@@ -0,0 +1,11 @@
+PROJECT_ID=destiny-app-23bc8
+
+gcloud builds submit dist --tag gcr.io/$PROJECT_ID/api \
+  --project $PROJECT_ID
+
+gcloud run deploy api --image gcr.io/$PROJECT_ID/api \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --set-env-vars=NODE_ENV=prod \
+  --project $PROJECT_ID
