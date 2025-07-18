b31bee873c11b8d23858ad5928c2fb122f807ca0
Gunnar Omander <gunnar.omander@gmail.com>
2025-07-18 23:15:10 +0200

    Update API

--- release-new-version.bat
+++ release-new-version.bat
@@ -2,13 +2,11 @@ git push
 git pull
 git checkout master
 
-echo off
-echo Increase version number in the file 'package.json'
-pause
-echo Confirm version number has been updated
-pause
+cd client
+pnpm version major
+cd ..
 for /f "delims=" %%i in ('
-  node -e "console.log(require('./client/package.json').version)"
+  node -e "console.log(require('./client/package.json').version.split('.')[0])"
 ') do set version=%%i
 
 git commit -a -m "Set v%version%"
