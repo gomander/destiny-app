0fab7d42c851923f5dee92ffda5a0194dfdbdcd8
Gunnar Omander <gunnar@gomander.dev>
2026-06-16 21:53:56 +0200

    Actually fix release script

--- release-new-version.sh
+++ release-new-version.sh
@@ -5,7 +5,7 @@ git checkout main
 pnpm version -r major
 version=$(node -e "console.log(require('./package.json').version.split('.')[0])")
 
-git commit -a -m "Set v%version%"
+git commit -a -m "Set v$version"
 git tag v$version
 git push
-git push origin v%version%
+git push origin v$version
